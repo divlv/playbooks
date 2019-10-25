99d252edbf0284938694cb69c284cc6a8fb18d8f
dimedrol <mail@me.com>
2019-10-26 02:00:38 +0300

    Firewall

--- docker_host/roles/docker_host/files/iptables.sh
+++ docker_host/roles/docker_host/files/iptables.sh
@@ -3,14 +3,9 @@
 # Erase all firewall rules
 iptables -F
 
-# Block null packets.
-iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
-
-# Reject is a syn-flood attack.
-iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
-
-# Common attack: XMAS packets, also a recon packet.
-iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
+# Block ALL by default
+iptables -P INPUT DROP
+# ... and open only specific ports:
 
 # Open up ports for selected services #############################
 # localhost interface
@@ -23,25 +18,19 @@ iptables -A FORWARD -i eth0 -o docker0 -j ACCEPT
 
 
 #allow web server traffic
-iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp -m multiport -m tcp --dport 80,443 -j ACCEPT
 
-#allow App server traffic from Home/Work
+# App server traffic from Home/Work
 iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 8080 -j ACCEPT
 iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 8080 -j ACCEPT
 
-#allow App server Administration traffic from Home/Work
+# Wildfly App server Administration traffic from Home/Work
 iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 9990 -j ACCEPT
 iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 9990 -j ACCEPT
 
 # SSH Home
-iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 22122 -j ACCEPT
-iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 22122 -j ACCEPT
-iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 22 -j ACCEPT
-
-# SSH Ansible host
-iptables -A INPUT -p tcp -s 185.185.68.178 -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp -s 159.148.74.220 -m multiport -m tcp --dport 22, 22122 -j ACCEPT
+iptables -A INPUT -p tcp -s 80.233.156.0/22 -m multiport -m tcp --dport 22, 22122 -j ACCEPT
 
 # PostgreSQL
 iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 5432 -j ACCEPT
@@ -56,12 +45,11 @@ iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 19000 -j ACCEPT
 iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 19000 -j ACCEPT
 
 
-# Allow o use outgoing connections
+# Allow outgoing connections for previously established incoming connections
 iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Block everything else, and allow all outgoing connections.
+# Allow all outgoing connections.
 iptables -P OUTPUT ACCEPT
-iptables -P INPUT DROP
 
 # List all rules
 iptables -L -n
