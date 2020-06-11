c036aaeb38bd00a4c89c9d8134f7feae758b0218
dimedrol <mail@me.com>
2020-06-11 14:19:25 +0300

    Firewall; Portainer

--- docker_host/roles/docker_host/files/iptables.sh
+++ docker_host/roles/docker_host/files/iptables.sh
@@ -30,48 +30,35 @@ iptables -A FORWARD -i eth0 -o docker0 -m state --state ESTABLISHED,RELATED -j A
 # Wildcard rule for Docker/User bridge networks, e.g. "br-f985e578d3a0"
 iptables -A FORWARD -i br-+ -o eth0 -j ACCEPT
 iptables -A FORWARD -i eth0 -o br-+ -m state --state ESTABLISHED,RELATED -j ACCEPT
-# 
+#
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 #
-# This opens Container's forwarding on new (U18) systems, 
+# This opens Container's forwarding on new (U18) systems,
 # and works like simply iptables forwarding between 2 interfaces
 #
 # MAIN PART ######################################################################
 
+ipset destroy trustedips
+ipset -N trustedips iphash
+#
+ipset -A trustedips 185.81.51.167
+###
+ipset destroy trustednets
+ipset -N trustednets nethash
+#
+ipset -A trustednets 80.233.156.0/22
+ipset -A trustednets 194.8.32.0/21
+ipset -A trustednets 194.8.40.0/22
+# ...PG for on-host Docker containers (private IP range)
+ipset -A trustednets 172.16.0.0/12
+
+
 # Web server traffic
 iptables -A INPUT -p tcp -m multiport -m tcp --dports 80,443 -j ACCEPT
 
-# App server traffic from Home/Work
-iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 8080 -j ACCEPT
-iptables -A INPUT -p tcp -s 185.81.51.167 -m tcp --dport 8080 -j ACCEPT
-iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 8080 -j ACCEPT
-
-# Wildfly App server Administration traffic from Home/Work
-iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 9990 -j ACCEPT
-iptables -A INPUT -p tcp -s 185.81.51.167 -m tcp --dport 9990 -j ACCEPT
-iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 9990 -j ACCEPT
-
-# SSH Home (including Ansible hub)
-iptables -A INPUT -p tcp -s 159.148.74.220 -m multiport -m tcp --dports 22,22122 -j ACCEPT
-iptables -A INPUT -p tcp -s 185.81.51.167 -m multiport -m tcp --dports 22,22122 -j ACCEPT
-iptables -A INPUT -p tcp -s 80.233.156.0/22 -m multiport -m tcp --dports 22,22122 -j ACCEPT
-
-# PostgreSQL
-iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 5432 -j ACCEPT
-iptables -A INPUT -p tcp -s 185.81.51.167 -m tcp --dport 5432 -j ACCEPT
-iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 5432 -j ACCEPT
-# ...PG for on-host Docker containers (private IP range)
-iptables -A INPUT -p tcp -s 172.16.0.0/12 -m tcp --dport 15432 -j ACCEPT
-
-# Monit
-iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 2812 -j ACCEPT
-iptables -A INPUT -p tcp -s 185.81.51.167 -m tcp --dport 2812 -j ACCEPT
-iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 2812 -j ACCEPT
-
-# Portainer
-iptables -A INPUT -p tcp -s 159.148.74.220 -m tcp --dport 19000 -j ACCEPT
-iptables -A INPUT -p tcp -s 185.81.51.167 -m tcp --dport 19000 -j ACCEPT
-iptables -A INPUT -p tcp -s 80.233.156.0/22 -m tcp --dport 19000 -j ACCEPT
+# Trusted
+iptables -A INPUT -p tcp -m multiport -m tcp -m set --match-set trustedips src --dports 22,2812,19000,18000,8080,9000,9090,9990,5432,15432,9323,6379,9273 -j ACCEPT
+iptables -A INPUT -p tcp -m multiport -m tcp -m set --match-set trustednets src --dports 22,2812,19000,18000,8080,9000,9090,9990,5432,15432,9323,6379,9273 -j ACCEPT
 
 ##################################################################################
 
