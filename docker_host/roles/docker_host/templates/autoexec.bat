0445f9adb81f9c0f9390ba345a96a1a67aaf6c5b
dimedrol <mail@me.com>
2021-06-09 11:45:56 +0300

    Autoexec.bat - Activate FloatingIP on reboot and other tasks if any

---
+++ docker_host/roles/docker_host/templates/autoexec.bat
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# AUTOEXEC.BAT :-) -=- This template file will be started each time on system reboot by CRON.
+#
+#
+
+# E.g. activate FloatingIP on reboot:
+#sudo ip addr add 1.2.3.4 dev eth0
