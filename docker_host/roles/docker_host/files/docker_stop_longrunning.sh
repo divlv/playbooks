4e9d651461674a960f67841a315434d4139dd289
dima <dima@zaharov.info>
2021-12-09 18:07:53 +0200

    Docker maintenance script

---
+++ docker_host/roles/docker_host/files/docker_stop_longrunning.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Stopping long running containers (filtered by image name part) (>2 min run)
+# E.g. MSSQL containers...
+#
+export CONTAINERFILTER=mssql
+#
+docker ps --format="{{.RunningFor}} {{.Names}} {{.Image}}" | grep $CONTAINERFILTER | grep minutes |  awk -F: '{if($1>2)print$1}' | awk ' {print $4} ' | xargs docker stop
+#
