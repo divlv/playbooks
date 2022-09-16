fe6c49be2ff2fc9c9af1a8398d35de7c2670c7c7
dima <dima@zaharov.info>
2022-09-16 14:30:26 +0300

    Manual JITSI tuning script

--- manual/jitsi.sh
+++ manual/jitsi.sh
@@ -18,3 +18,9 @@ curl https://download.jitsi.org/jitsi-key.gpg.key | sudo sh -c 'gpg --dearmor > 
 echo 'deb [signed-by=/usr/share/keyrings/jitsi-keyring.gpg] https://download.jitsi.org stable/' | sudo tee /etc/apt/sources.list.d/jitsi-stable.list > /dev/null
 
 apt update
+
+apt install -y jitsi-meet
+
+# Then better reboot the system and add LetsEncrypt certificate:
+# sudo /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh
+# And reboot again
