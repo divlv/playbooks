02ec46d22b11dea522c95e7d87eed891bdaeb99d
dima <dima@zaharov.info>
2022-09-16 14:28:31 +0300

    Manual JITSI tuning script

---
+++ manual/jitsi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#
+#
+# ( https://jitsi.github.io/handbook/docs/devops-guide/devops-guide-quickstart/ )
+#
+# JITSI - Packages add
+#
+
+echo deb http://packages.prosody.im/debian $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list
+
+wget https://prosody.im/files/prosody-debian-packages.key -O- | sudo apt-key add -
+
+apt install -y lua5.2
+
+curl https://download.jitsi.org/jitsi-key.gpg.key | sudo sh -c 'gpg --dearmor > /usr/share/keyrings/jitsi-keyring.gpg'
+
+echo 'deb [signed-by=/usr/share/keyrings/jitsi-keyring.gpg] https://download.jitsi.org stable/' | sudo tee /etc/apt/sources.list.d/jitsi-stable.list > /dev/null
+
+apt update
