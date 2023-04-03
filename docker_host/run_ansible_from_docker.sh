87fb1349504ddef80843e914321f03838b20b8d6
dima <dima@zaharov.info>
2023-04-03 20:18:54 +0300

    Dockerized Ansible - all files mapped

--- docker_host/run_ansible_from_docker.sh
+++ docker_host/run_ansible_from_docker.sh
@@ -6,16 +6,15 @@
 
 # A path where this playbook was checked out from GitHub...
 export PLAYBOOK_DIR="/path/to/playbooks/docker_host"
-
-# Copy key file to the playbook directory
+# Fisrt, copy key file to the playbook directory!
 export PRIVATE_KEY_FILE_NAME="private.key"
-export PRIVATE_KEY_FILE_MAPPED_PATH="/p/$PRIVATE_KEY_FILE_NAME"
-
-export PLAYBOOK_TO_RUN="$PLAYBOOK_DIR/azure_playbook.yml"
+export PLAYBOOK_TO_RUN="azure_playbook.yml"
 
 # Make key file accepable by Ansible
 chmod 600 $PLAYBOOK_DIR/$PRIVATE_KEY_FILE_NAME
 
 docker run -it \
 -v $PLAYBOOK_DIR:/p \
-cytopia/ansible:2.8-tools /bin/sh -c 'cd /p; ansible-playbook --private-key $PRIVATE_KEY_FILE_MAPPED_PATH -i /p/hosts $PLAYBOOK_TO_RUN'
+-v $PLAYBOOK_DIR/$PRIVATE_KEY_FILE_NAME:/p/private.key \
+-v $PLAYBOOK_DIR/$PLAYBOOK_TO_RUN:/p/playbook.yml \
+cytopia/ansible:2.8-tools /bin/sh -c 'cd /p; ansible-playbook --private-key /p/private.key -i /p/hosts /p/playbook.yml'
