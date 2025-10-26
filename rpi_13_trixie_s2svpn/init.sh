32668d2fa9400a87138b63eb756c55f0f78d28df
dmitrijszaharovs <dmitrijs.zaharovs@emergn.com>
2025-10-26 10:04:20 +0200

    Init scripts for Raspbian GNU/Linux 13 (trixie) Lite

---
+++ rpi_13_trixie_s2svpn/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#
+# Init commands for "Raspbian GNU/Linux 13 (trixie) Lite" to be used as Azure S2S On-Prem VPN Gateway
+#
+
+sudo apt update
+
+sudo apt install -y strongswan strongswan-pki strongswan-starter netcat mc ncdu tcpdump
