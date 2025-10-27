68979ee5bc35d0835fa38ab848539c1839b44cf0
dmitrijszaharovs <dmitrijs.zaharovs@emergn.com>
2025-10-27 20:42:11 +0200

    Update .gitignore and enhance update_ipsec.sh to replace placeholders in IPsec configuration files

--- rpi_13_trixie_s2svpn/update_ipsec.sh
+++ rpi_13_trixie_s2svpn/update_ipsec.sh
@@ -12,3 +12,11 @@ AZURE_RESOURCES_VNET_1="10.82.4.0/22"
 
 AZURE_VPN_GATEWAY_IP_1="20.30.40.50"
 AZURE_VPN_GATEWAY_IP_2="20.30.40.51"
+
+# Find all ipsec.* files recursively and replace placeholders
+find . -type f -name "ipsec.*" -exec sed -i "s|#LOCAL_VNET_DEVICE_EXTERNAL_IP#|${LOCAL_VNET_DEVICE_EXTERNAL_IP}|g" {} \;
+find . -type f -name "ipsec.*" -exec sed -i "s|#LOCAL_INTERNAL_SUBNET#|${LOCAL_INTERNAL_SUBNET}|g" {} \;
+find . -type f -name "ipsec.*" -exec sed -i "s|#AZURE_VPN_VNET#|${AZURE_VPN_VNET}|g" {} \;
+find . -type f -name "ipsec.*" -exec sed -i "s|#AZURE_RESOURCES_VNET_1#|${AZURE_RESOURCES_VNET_1}|g" {} \;
+find . -type f -name "ipsec.*" -exec sed -i "s|#AZURE_VPN_GATEWAY_IP_1#|${AZURE_VPN_GATEWAY_IP_1}|g" {} \;
+find . -type f -name "ipsec.*" -exec sed -i "s|#AZURE_VPN_GATEWAY_IP_2#|${AZURE_VPN_GATEWAY_IP_2}|g" {} \;
